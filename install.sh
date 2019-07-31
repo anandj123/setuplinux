362567fe4fd1bc53413e2b60ad009746efacd2ac
Anand Jain <jain.12@gmail.com>
2019-07-31 16:36:39 -0400

    Commit 7/31/2019

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# NAME: install
+
+sudo apt-get update
+sudo apt-get install unzip
+sudo apt-get install wget gnupg gnupg1 gnupg2
+sudo apt-get install maven
+sudo apt install screenfetch
+sudo apt install toilet
+sudo apt install figlet
+sudo apt-get update
